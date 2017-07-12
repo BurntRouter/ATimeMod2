f32521c3d0e5bc82198b8ed2c6b34e5a67fd816f
Burnt Router <router@burntrouter.info>
2017-07-12 14:20:07 -0400

    Items and textures moved into ATimeMod2

---
+++ src/main/java/com/Router1011/ATimeMod2/ATimeTab.java
@@ -0,0 +1,24 @@
+package com.Router1011.ATimeMod2;
+
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import com.Router1011.ATimeMod2.Items.ATimeItems;
+
+public class ATimeTab extends CreativeTabs{
+
+	public ATimeTab() {
+		super(ATimeMod2.modId);
+	}
+
+	@Override
+	public Item getTabIconItem() {
+		return ATimeItems.Scarlet;
+	}
+	
+	 @Override
+	 public boolean hasSearchBar() {
+		 return true;
+	 }
+
+}
--- src/main/java/com/Router1011/ATimeMod2/Items/ATimeItems.java
+++ src/main/java/com/Router1011/ATimeMod2/Items/ATimeItems.java
@@ -5,12 +5,49 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ATimeItems {
-	public static ItemBase ingotCopper;
+	public static ItemBase ApplePie, Bacon, BaconPancake, BassAxe, BillyGauntlet, BubbleGum,
+	Candy, CottonCandy, CrystalAxe, CrystalGem, CrystalHoe, CrystalPickaxe, CrystalShovel, CrystalSword,
+	DemonBlood, DemonIngot, DemonSword, Donut, Enchiridion, EverythingBurrito, Flour, FourDSword, GemApple,
+	GrassSword, IceShard, JakeBoots, JakeChest, JakeHead, JakeLegs, JakeSword, Lollipop, Pancake, PaperSword,
+	RootSword, Scarlet, SteelIngot;
 	 
 	 public static void init() {
 		 
-	ingotCopper = register(new ItemBase("ingotCopper").setCreativeTab(CreativeTabs.MATERIALS));
-	
+	ApplePie = register(new ItemBase("ApplePie").setCreativeTab(CreativeTabs.MATERIALS));
+	Bacon = register(new ItemBase("Bacon").setCreativeTab(CreativeTabs.MATERIALS));
+	BaconPancake = register(new ItemBase("BaconPancake").setCreativeTab(CreativeTabs.MATERIALS));
+	BassAxe = register(new ItemBase("BassAxe").setCreativeTab(CreativeTabs.MATERIALS));
+	BillyGauntlet = register(new ItemBase("BillyGauntlet").setCreativeTab(CreativeTabs.MATERIALS));
+	BubbleGum = register(new ItemBase("BubbleGum").setCreativeTab(CreativeTabs.MATERIALS));
+	Candy = register(new ItemBase("Candy").setCreativeTab(CreativeTabs.MATERIALS));
+	CottonCandy = register(new ItemBase("CottonCandy").setCreativeTab(CreativeTabs.MATERIALS));
+	CrystalAxe = register(new ItemBase("CrystalAxe").setCreativeTab(CreativeTabs.MATERIALS));
+	CrystalGem = register(new ItemBase("CrystalGem").setCreativeTab(CreativeTabs.MATERIALS));
+	CrystalHoe = register(new ItemBase("CrystalHoe").setCreativeTab(CreativeTabs.MATERIALS));
+	CrystalPickaxe = register(new ItemBase("CrystalPickaxe").setCreativeTab(CreativeTabs.MATERIALS));
+	CrystalShovel = register(new ItemBase("CrystalShovel").setCreativeTab(CreativeTabs.MATERIALS));
+	CrystalSword = register(new ItemBase("CrystalSword").setCreativeTab(CreativeTabs.MATERIALS));
+	DemonBlood = register(new ItemBase("DemonBlood").setCreativeTab(CreativeTabs.MATERIALS));
+	DemonIngot = register(new ItemBase("DemonIngot").setCreativeTab(CreativeTabs.MATERIALS));
+	DemonSword = register(new ItemBase("DemonSword").setCreativeTab(CreativeTabs.MATERIALS));
+	Donut = register(new ItemBase("Donut").setCreativeTab(CreativeTabs.MATERIALS));
+	Enchiridion = register(new ItemBase("Enchiridion").setCreativeTab(CreativeTabs.MATERIALS));
+	EverythingBurrito = register(new ItemBase("EverythingBurrito").setCreativeTab(CreativeTabs.MATERIALS));
+	Flour = register(new ItemBase("Flour").setCreativeTab(CreativeTabs.MATERIALS));
+	GemApple = register(new ItemBase("GemApple").setCreativeTab(CreativeTabs.MATERIALS));
+	GrassSword = register(new ItemBase("GrassSword").setCreativeTab(CreativeTabs.MATERIALS));
+	IceShard = register(new ItemBase("IceShard").setCreativeTab(CreativeTabs.MATERIALS));
+	JakeBoots = register(new ItemBase("JakeBoots").setCreativeTab(CreativeTabs.MATERIALS));
+	JakeChest = register(new ItemBase("JakeChest").setCreativeTab(CreativeTabs.MATERIALS));
+	JakeHead = register(new ItemBase("JakeHead").setCreativeTab(CreativeTabs.MATERIALS));
+	JakeLegs = register(new ItemBase("JakeLegs").setCreativeTab(CreativeTabs.MATERIALS));
+	JakeSword = register(new ItemBase("JakeSword").setCreativeTab(CreativeTabs.MATERIALS));
+	Lollipop = register(new ItemBase("Lollipop").setCreativeTab(CreativeTabs.MATERIALS));
+	Pancake = register(new ItemBase("Pancake").setCreativeTab(CreativeTabs.MATERIALS));
+	PaperSword = register(new ItemBase("PaperSword").setCreativeTab(CreativeTabs.MATERIALS));
+	RootSword = register(new ItemBase("RootSword").setCreativeTab(CreativeTabs.MATERIALS));
+	Scarlet = register(new ItemBase("Scarlet").setCreativeTab(CreativeTabs.MATERIALS));
+	SteelIngot = register(new ItemBase("SteelIngot").setCreativeTab(CreativeTabs.MATERIALS));	
 	 };
 
 	
