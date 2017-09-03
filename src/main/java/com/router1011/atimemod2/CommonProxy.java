add7f812cb040e97032f39a9232a9ece9428ca65
Burnt Router <router@burntrouter.info>
2017-09-02 20:33:46 -0400

    Registered Ore Generation

--- src/main/java/com/router1011/atimemod2/CommonProxy.java
+++ src/main/java/com/router1011/atimemod2/CommonProxy.java
@@ -5,11 +5,12 @@ import com.router1011.atimemod2.entity.ATimeLivingEntities;
 import com.router1011.atimemod2.gui.ATimeGuis;
 import com.router1011.atimemod2.items.ATimeItems;
 import com.router1011.atimemod2.recipes.ATimeRecipes;
-import com.router1011.atimemod2.world.WorldOreGen;;
+import com.router1011.atimemod2.world.WorldOreGen;
 
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 
 public class CommonProxy {
@@ -24,6 +25,7 @@ public class CommonProxy {
 
 	public void init(FMLInitializationEvent e) {
 		OreDictionary.registerOre("ingotSteel", ATimeItems.SteelIngot);
+		GameRegistry.registerWorldGenerator(new WorldOreGen(), 0);
 	}
 
 	public void postInit(FMLPostInitializationEvent e) {
