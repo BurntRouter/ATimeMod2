31c1e02db23cecafe23d35ce2611bdc6addfff1c
Koceila Beddek <kbeddek@gmail.com>
2018-09-05 16:07:03 -0400

    added blocks

--- src/main/java/com/router/atimemod2/blocks/ATimeBlocks.java
+++ src/main/java/com/router/atimemod2/blocks/ATimeBlocks.java
@@ -19,6 +19,10 @@ public class ATimeBlocks {
 		public static BlockBase candy_log = new BlockBase(Material.WOOD, "candy_log", 1.2F, "axe", 0, SoundType.WOOD, 1.2F);
 		public static CandyLeaves candy_leaves = new CandyLeaves();
 		public static CandySapling candy_sapling = new CandySapling();
+		public static BlockBase ooo_log = new BlockBase(Material.WOOD, "ooo_log", 1.0F, "axe", 0, SoundType.WOOD, 1.2F);
+		public static BlockBase ooo_leaves = new BlockBase(Material.LEAVES, "ooo_leaves", 0.4F, "axe", 0, SoundType.PLANT, 0.1F);
+		public static BlockBase cottoncandy_log = new BlockBase(Material.WOOD, "cottoncandy_log", 1.0F, "axe", 0, SoundType.WOOD, 1.2F);
+		public static BlockBase cottoncandy_leaves = new BlockBase(Material.LEAVES, "cottoncandy_leaves", 0.4F, "axe", 0 ,SoundType.PLANT, 0.1F);
 		
 		public static void register(IForgeRegistry<Block> registry) {
 			registry.registerAll(
@@ -28,7 +32,11 @@ public class ATimeBlocks {
 					crystal_ore,
 					candy_log,
 					candy_leaves,
-					candy_sapling
+					candy_sapling,
+					ooo_log,
+					ooo_leaves,
+					cottoncandy_log,
+					cottoncandy_leaves
 			);
 		}
 		
@@ -40,7 +48,11 @@ public class ATimeBlocks {
 					crystal_ore.createItemBlock(),
 					candy_log.createItemBlock(),
 					candy_leaves.createItemBlock(),
-					candy_sapling.createItemBlock()
+					candy_sapling.createItemBlock(),
+					ooo_log.createItemBlock(),
+					ooo_leaves.createItemBlock(),
+					cottoncandy_log.createItemBlock(),
+					cottoncandy_leaves.createItemBlock()
 			);
 		}
 		
@@ -52,5 +64,9 @@ public class ATimeBlocks {
 			candy_log.registerItemModel(Item.getItemFromBlock(candy_log));
 			candy_leaves.registerItemModel(Item.getItemFromBlock(candy_leaves));
 			candy_sapling.registerItemModel(Item.getItemFromBlock(candy_sapling));
+			ooo_log.registerItemModel(Item.getItemFromBlock(ooo_log));
+			ooo_leaves.registerItemModel(Item.getItemFromBlock(ooo_leaves));
+			cottoncandy_log.registerItemModel(Item.getItemFromBlock(cottoncandy_log));
+			cottoncandy_leaves.registerItemModel(Item.getItemFromBlock(cottoncandy_leaves));
 		}
 }
--- src/main/java/com/router/atimemod2/blocks/CandyLog.java
+++ src/main/java/com/router/atimemod2/blocks/CandyLog.java
@@ -1,13 +1,18 @@
 package com.router.atimemod2.blocks;
 
+import com.router.atimemod2.ATimeMod;
+
 import net.minecraft.block.BlockLog;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
@@ -15,78 +20,26 @@ import net.minecraft.world.IBlockAccess;
 
 public class CandyLog extends BlockLog
 {
-
-    public CandyLog()
-    {
-        super();
-        setDefaultState(blockState.getBaseState().withProperty(LOG_AXIS, BlockLog.EnumAxis.Y));
-
-    }
+	protected String name;
+	
+    public CandyLog(Material material, String name, Float hardness, String toolClass, int level, SoundType soundType, Float resistance) {
+		super();
+		this.name = name;
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(ATimeMod.creativeTab);
+		setHardness(hardness);
+		setHarvestLevel(toolClass, level);
+		setResistance(resistance);
+		setSoundType(soundType);
+	}
     
-    @Override
-    public MapColor getMapColor(IBlockState state, IBlockAccess worldIn, BlockPos pos)
-    {
-        return Blocks.LOG.getDefaultState().getMapColor(worldIn, pos);
-    }
+	public void registerItemModel(Item itemBlock) {
+		ATimeMod.proxy.registerItemRenderer(itemBlock, 0, name);
+	}
+	
+	public Item createItemBlock() {
+		return new ItemBlock(this).setRegistryName(getRegistryName());
+	}
 
-    public void getSubBlocks(CreativeTabs itemIn, NonNullList<ItemStack> items)
-    {
-        items.add(new ItemStack(this));
-    }
-
-    @Override
-    public IBlockState getStateFromMeta(int meta)
-    {
-        IBlockState state = this.getDefaultState();
-
-        switch (meta & 12)
-        {
-            case 0:
-                state = state.withProperty(LOG_AXIS, BlockLog.EnumAxis.Y);
-                break;
-            case 4:
-                state = state.withProperty(LOG_AXIS, BlockLog.EnumAxis.X);
-                break;
-            case 8:
-                state = state.withProperty(LOG_AXIS, BlockLog.EnumAxis.Z);
-                break;
-            default:
-                state = state.withProperty(LOG_AXIS, BlockLog.EnumAxis.NONE);
-        }
-
-        return state;
-    }
-
-    @Override
-    @SuppressWarnings("incomplete-switch")
-    public int getMetaFromState(IBlockState state)
-    {
-        int meta = 0;
-
-        switch (state.getValue(LOG_AXIS))
-        {
-            case X:
-                meta |= 4;
-                break;
-            case Z:
-                meta |= 8;
-                break;
-            case NONE:
-                meta |= 12;
-        }
-
-        return meta;
-    }
-
-    @Override
-    protected BlockStateContainer createBlockState()
-    {
-        return new BlockStateContainer(this, new IProperty[] {LOG_AXIS});
-    }
-
-    @Override
-    public int damageDropped(IBlockState state)
-    {
-        return 0;
-    }
 }
